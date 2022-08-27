b23fed6880dcb102921907f87a23dd6247f062f2
Josh S <josh@operationspark.org>
2022-08-27 12:45:25 -0500

    fixing routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -10,7 +10,7 @@ breads.get('/', async (req, res) => {
   try {
     const foundBakers = await Baker.find().lean();
     const foundBreads = await Bread.find().lean();
-    res.render('/index', {
+    res.render('Index', {
       breads: foundBreads,
       bakers: foundBakers,
       title: 'Index Page',
